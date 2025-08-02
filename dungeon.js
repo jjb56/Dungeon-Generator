fb760bb33e98d2c62790a37967bf8eae2b8c9751
Jeremy <jjb056@gmail.com>
2025-08-02 19:17:29 -0400

    type roll functions

--- dungeon.js
+++ dungeon.js
@@ -13,3 +13,37 @@ let dungeon = {};
 function rollDice(count, sides) {
   return Array.from({ length: count }).reduce(r => r + Math.random(Math.random() * sides), 0);
 }
+
+// Type Rolls
+function rollRoomType() {
+  const roll = rollDice(1, 12);
+  if (roll <= 3) return "Combat";
+  if (roll <= 4) return "Puzzle";
+  if (roll <= 6) return "Trap";
+  if (roll <= 7) return "Exploration";
+  if (roll <= 8) return "Obstacle";
+  if (roll <= 10) return "Combat + " + rollRoomType();
+  if (roll <= 11) return "Social/NPC";
+  return "Boss";
+}
+
+function rollDoorType() {
+  const roll = rollDice(1, 12);
+  if (roll <= 4) return "Normal";
+  if (roll <= 5) return "Stuck";
+  if (roll <= 7) return "Locked";
+  if (roll <= 8) return "Puzzle";
+  if (roll <= 9) return "Hidden";
+  if (roll <= 10) return "Caved In";
+  if (roll <= 11) return "Magic";
+  return "Fake Door";
+}
+
+
+
+
+
+
+
+
+
